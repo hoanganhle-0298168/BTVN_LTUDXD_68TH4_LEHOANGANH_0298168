08df336421d53dce813f5db7565205c7585ea2b9
Your Namehoangle2-dev <le5652866@gmail.com>
2026-03-28 22:27:08 +0700

    Xong bài 3

---
+++ Bai_Tap_3/Bai_Tap_3/View/UC_Giao_Dien.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bai_Tap_3.View
+{
+    /// <summary>
+    /// Interaction logic for UC_Giao_Dien.xaml
+    /// </summary>
+    public partial class UC_Giao_Dien : UserControl
+    {
+        public UC_Giao_Dien()
+        {
+            InitializeComponent();
+        }
+    }
+}
